52fc36e037fb9b1b4dc11440365819eefbe29dd5
yourDoudou <163140169+yourDoudou@users.noreply.github.com>
2024-05-15 18:11:42 +0200

    Update Personne.java

--- ArbreGenealogique/src/Personne.java
+++ ArbreGenealogique/src/Personne.java
@@ -2,4 +2,12 @@
 public class Personne {
   private int id;
   private String nom;
+  private String prenom;
+  private String sexe;
+  private dateDeNaissance Date;
+  private dateDeDeces Date;
+  private pere Personne;
+  private mere Personne;
+  private enfants List<Personne>;
+  
 }
